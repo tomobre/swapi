703a83e9412b235f9c97a8aebfbefb3d1aff3da5
Tomas Obregon <tomasobregon@MacBook-Air-de-Tomas.local>
2022-01-09 18:48:47 -0300

    started project, structure finsihed, style desktop finished.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PlanetList from "./components/PlanetList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import styled from "styled-components";
+
+/* const PLANETS = gql`
+  query Planets {
+    allPlanets {
+      totalCount
+    }
+  }
+`; */
+
+const PLANETS = gql`
+  {
+    characters {
+      results {
+        id
+        name
+      }
+    }
+  }
+`;
+
+const WrapperStatus = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  margin: 2rem;
+`;
 
 function App() {
+  const [planets, setPlanets] = React.useState([]);
+  const { data, loading, error } = useQuery(PLANETS, {
+    onCompleted: () => {
+      let result = data.characters.results.map((char) => {
+        return Object.assign(char, { favourite: false });
+      });
+
+      setPlanets(result);
+    },
+  });
+
+  if (loading) {
+    return <WrapperStatus>Cargando...</WrapperStatus>;
+  }
+
+  if (error) {
+    return (
+      <WrapperStatus>
+        Upss...hubo un error! Vuelva a intentarlo mas tarde.
+      </WrapperStatus>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<PlanetList setPlanets={setPlanets} planets={planets} />}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/PlanetList.js
@@ -0,0 +1,118 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 2rem;
+`;
+const TopWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+`;
+const Input = styled.input`
+font-size: 15px;
+  border: none;
+ box-shadow: inset 0 -1px 0 rgba(#000,.3);
+    color: #000;
+    transition: all .15s ease
+
+  margin: 0rem 1rem;
+`;
+const Headers = styled.th`
+  padding: 1rem;
+  font-size: 15px;
+  margin: 1rem;
+  background-color: #cecccc;
+`;
+const TData = styled.td`
+  padding: 1rem;
+  font-size: 13px;
+  margin: 1rem;
+  background-color: #f4f4f4;
+`;
+
+function PlanetList({ planets, setPlanets }) {
+  const [favourites, setFavourites] = React.useState(false);
+
+  return (
+    <Wrapper>
+      <TopWrapper>
+        {" "}
+        <Input type="text" placeholder="Buscar por nombre..." />
+        {favourites ? (
+          <Button
+            onClick={() => {
+              setFavourites(false);
+            }}
+          >
+            Volver
+          </Button>
+        ) : (
+          <Button
+            onClick={() => {
+              setFavourites(true);
+            }}
+          >
+            Ver favoritos
+          </Button>
+        )}
+      </TopWrapper>
+
+      <table>
+        <tbody>
+          <tr>
+            <Headers>Nombre</Headers>
+            <Headers>Diametro</Headers>
+            <Headers> Clima</Headers>
+            <Headers> Terreno</Headers>
+            <Headers>Favorito</Headers>
+          </tr>
+          {planets.map((planet, index) => {
+            let html = (
+              <tr key={planet.id}>
+                <TData>{planet.name}</TData>
+                <TData>{planet.name}</TData>
+                <TData>{planet.name}</TData>
+                <TData>{planet.name}</TData>
+                <TData>
+                  {" "}
+                  <input
+                    type="checkbox"
+                    checked={planet.favourite}
+                    onChange={(e) => {
+                      let newArr = [...planets];
+                      newArr[index].favourite = e.target.checked;
+                      setPlanets(newArr);
+                    }}
+                  />
+                </TData>
+              </tr>
+            );
+
+            if (favourites) {
+              return planet.favourite && html;
+            } else {
+              return html;
+            }
+          })}
+        </tbody>
+      </table>
+    </Wrapper>
+  );
+}
+
+export default PlanetList;
